5280640ca7c3027f87e89c493d626d2e629e25aa
Isoraķatheð Zorethan <isoraqathedh.zorethan@gmail.com>
2016-02-09 17:48:30 +0000

    Update to use YAML and cl-yaclyaml

--- books-and-pages.lisp
+++ books-and-pages.lisp
@@ -8,7 +8,7 @@ Classes and methods that relate to representing and finding books and pages.
 
 ;;; Basic variables and helper functions
 (defvar *config-file* (asdf:system-relative-pathname
-                       :bocproc "config" :type "json"))
+                       :bocproc "config" :type "yaml"))
 
 (defparameter *books-location*
   (merge-pathnames
@@ -16,12 +16,26 @@ Classes and methods that relate to representing and finding books and pages.
    (uiop/common-lisp:user-homedir-pathname)))
 
 (defparameter *config* ()
-  "Configuration sexpr.")
+  "Configuration hash-table.")
+
+(defun number->letter (number)
+  (ecase number
+    (1  #\a) (2  #\b) (3  #\c) (4  #\d) (5  #\e)
+    (6  #\f) (7  #\g) (8  #\h) (9  #\i) (10 #\j)
+    (11 #\k) (12 #\l) (13 #\m) (14 #\n) (15 #\o)
+    (16 #\p) (17 #\q) (18 #\r) (19 #\s) (20 #\t)
+    (21 #\u) (22 #\v) (23 #\w) (24 #\x) (25 #\y) (26 #\z)))
 
 (defun load-config-file ()
   "Reads the config file into *config*."
   (with-open-file (s *config-file* :external-format :utf-8)
-    (setf *config* (cl-json:decode-json s))))
+    (setf *config*
+          (-> s
+            uiop:slurp-stream-string
+            cl-yy:yaml-load
+            car
+            cadr
+            ))))
 
 (defun books-location-subdir (&rest folder-names)
   (merge-pathnames
@@ -119,35 +133,50 @@ Raises an error and gives common resolutions.")
   (:documentation "Makes the pathname for the book.
 Raises an error if the book is not specific enough.")
   (:method ((book page))
-    (ensure-book-specific book :book)
-    ))
+    #+(or) (ensure-book-specific book :book)
+    (with-accessors ((book book) (root root)) book
+      (merge-pathnames
+       (make-pathname :directory (list :relative (format nil "Book ~a" book)))
+       root))))
+
+(defgeneric page-path-wild (page)
+  (:documentation "Makes the pathname for a specific page.
+Only returns the wild format. ")
+  (:method ((page-object book-of-conworlds-page))
+    #+(or) (ensure-book-specific book :page)
+    (with-accessors ((page page) (subpage subpage)) page-object
+      (merge-pathnames
+       (make-pathname :name (pathname-name
+                             (format nil "~a~a-*"
+                                     page
+                                     (if subpage (number->letter subpage) "?")))
+                      :type "jpg")
+       (book-path page-object)))))
 
 (defgeneric book-exists (book)
   (:documentation "Checks if a book exists.
  Raises errors if the book is not specific enough.")
-  (:method ((book number))
-    ())
-  #+(or) (:method ((book book))
-           (unless (specificity<= :book (specificity book))
-             (error 'not-specific-enough :thing ))))
+  (:method ((page-object page))
+    #+(or) (ensure-book-specific page-object :book)
+    (probe-file (book-path page-object))))
 
 (defgeneric book-ignored-p (book)
   (:documentation "Checks if a book is ignored.")
-  (:method ((book number))
-    (->> *config*
-      (assoc :ignored) cdr
-      (assoc :boc) cdr
-      (nth (1- book))
-      (eql t)))
-  (:method ((book page))
-    (book-ignored-p (book book))))
+  (:method ((book-object book-of-conworlds-page))
+    (with-accessors ((book book)) book-object
+      (->> *config*
+        (gethash "ignored")
+        (gethash "boc")
+        (nth (1- book))
+        (eql t)))))
 
-(defgeneric page-ignored-p (book page)
+(defgeneric page-ignored-p (page)
   (:documentation "Checks if a book is ignored.")
-  (:method ((book number) (page number))
-    (with-expression-threading (*config*)
-      (assoc :ignored :||) cdr
-      (assoc :boc :||) cdr
-      (nth (1- book) :||)
-      (and (listp :||)
-           (member page :||)))))
+  (:method ((page-object page))
+    (with-accessors ((page page) (book book)) page-object
+      (with-expression-threading (*config*)
+        (gethash "ignored" :||)
+        (gethash "boc" :||)
+        (nth (1- book) :||)
+        (and (listp :||)
+             (member page :||))))))
