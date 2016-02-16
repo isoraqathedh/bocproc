a148581f10a0b565f574ad870c745f0fa5c6551e
Isoraķatheð Zorethan <isoraqathedh.zorethan@gmail.com>
2016-02-16 22:50:28 +0000

    Various major changes
    - Moved all the specificity functions into one place
    - Documented them
    - Allows disabling of checking (for profiling)
    - Filename creation now modular (still broken)

--- books-and-pages.lisp
+++ books-and-pages.lisp
@@ -8,7 +8,6 @@ Classes and methods that relate to representing and finding books and pages.
 ;;; Generic classes for a page.
 (defclass page ()
   ((page-number-list :initarg :page-numbers
-                     :initform (list)
                      :accessor page-numbers
                      :documentation "A list of numbers
 that represent the book/page/subpage structure of the page.")
@@ -57,6 +56,7 @@ which is not specific enough for ~a."
                      (datum condition)
                      (target condition)))))
 
+;;; Mixins
 (defclass dated-page ()
   ((date-of-creation :accessor date-of-creation))
   (:documentation "Mixin of pages
@@ -72,7 +72,7 @@ whose file names depend on wildcards."))
    (root-path :initform (books-location-subdir "Book of Conworlds")))
   (:documentation "Represents a page in the book of conworlds book."))
 
-(defclass non-boc-conworld-page (dated-page page-using-wildcards page)
+(defclass non-boc-conworld-page (dated-page page-using-wildcards book-of-conworlds-page)
   ((specificities :initform '(:page :subpage))
    (root-path :initform (books-location-subdir "Book of Conworlds" "Non-BoC")))
   (:documentation "Represents a book of conworlds page
@@ -84,32 +84,88 @@ without an associated book."))
   (:documentation "Represents another scan."))
 
 ;;; Printing controls
-(defun format-time (timestamp)
-  (local-time:format-timestring
-   nil timestamp :format '(:year "-" (:month 2) " (" :short-month ")")))
 
 (defmethod initialize-instance :after
     ((instance dated-page) &key &allow-other-keys)
   (setf (date-of-creation instance) (local-time:now)))
 
+(defmethod initialize-instance :after
+    ((instance page) &key page-numbers &allow-other-keys)
+  (setf (page-numbers instance)
+        (loop with array = (make-array (length (specificities instance))
+                                       :element-type '(or fixnum null)
+                                       :initial-element nil)
+              for number in page-numbers
+              for counter from 0
+              do (setf (aref array counter) number)
+              finally (return array))))
+
+;;; Specificities
+;;; = = = = = = =
+#| This is an important part of a book.
+A specificity is how, er, specific a page object is.
+That is, how many out of all possible dimensions it needs to define
+in order to exactly pinpoint a location in page-space are so.
+For instance, with specificities (:FOO :BAR :BAZ :QUUX),
+There are five total specificities (all four of the above and also NIL)
+corresponding to the following:
+
+NIL: #(NIL NIL NIL NIL)
+FOO: #(# NIL NIL NIL)
+BAR: #(# # NIL NIL)
+BAZ: #(# # # NIL)
+QUUX: #(# # # #)
+
+(Where # represents an arbitrary number)
+
+It is implicit that the specificity of the last element
+will be able to uniquely identify the file name that the page corresponds to.
+
+Certain functions will only work when an object is specific enough.
+To ensure that the object is specific enough, use `ensure-book-specific'.
+|#
+(defgeneric get-specificity (object specificity)
+  (:documentation "Retrieves the appropriate page number
+with the provided specificity.")
+  (:method ((object page) specificity)
+    (aref (page-numbers object) (position specificity (specificities object)))))
+
+(defgeneric (setf get-specificity) (value object specificity)
+  (:method (value (object page) specificity)
+    (setf (aref (page-numbers object)
+                (position specificity (specificities object)))
+          value)))
+
+(defmacro specificity-bind ((&rest vars-and-specs) object &body body)
+  "Binds the specificities of a page to variables."
+  (let ((object^ (gensym)))
+    `(let ((,object^ ,object))
+       (symbol-macrolet ,(loop for (var spec) in vars-and-specs
+                               collect `(,var (get-specificity ,object^ ,spec)))
+         ,@body))))
+
+;;; Printing controls
+(defun format-time (timestamp)
+  (local-time:format-timestring
+   nil timestamp :format '(:year "-" (:month 2) " (" :short-month ")")))
+
 (defgeneric format-page-code (page)
   (:documentation "Writes the printed representation of a page object.")
-  (:method ((page book-of-conworlds-page))
-    (destructuring-bind (&optional book page subpage) (page-numbers page)
+  (:method ((object book-of-conworlds-page))
+    (specificity-bind ((book :book) (page :page) (subpage :subpage)) object
       (format nil "~:[?~;~:*~d~] / ~:[??~;~:*~2,'0d~]~:[?~;~:*~c~]"
-              book
-              page
-              (number->letter subpage))))
+              book page (number->letter subpage))))
   (:method ((page-object non-boc-conworld-page))
-    (destructuring-bind (&optional page subpage) (page-numbers page-object)
+    (specificity-bind ((page :page) (subpage :subpage)) page-object
       (format nil "~a / ~:[??~;~:*~2,'0d~]~:[?~;~:*~c~]"
               (format-time (date-of-creation page-object))
               page
               (number->letter subpage))))
   (:method ((page-object non-boc-page))
-    (format nil "~a / ~:[????~;~:*~4,'0d~]"
-            (format-time (date-of-creation page-object))
-            (first (page-numbers page-object)))))
+    (specificity-bind ((page :page)) page-object
+        (format nil "~a / ~:[????~;~:*~4,'0d~]"
+             (format-time (date-of-creation page-object))
+             page))))
 
 (defmethod print-object ((object page) stream)
   (print-unreadable-object (object stream :type t)
@@ -119,43 +175,15 @@ without an associated book."))
   (print-unreadable-object (object stream :type t :identity t)
     (format stream "~a ~a" (root object) (page-numbers object))))
 
-;;; Operations
-;;; = = = = =
-
-;;; Specificity requirements
-(defgeneric get-specificity (object specificity)
-  (:documentation "Retrieves the appropriate page number
-with the provided specificity.")
-  (:method ((object page) specificity)
-    (nth (position specificity (specificities object)) (page-numbers object))))
-
-(defgeneric (setf get-specificity) (value object specificity)
-  (:method (value (object page) specificity)
-    (setf (page-numbers object)
-          (loop with page-number-list = (page-numbers object)
-                with list-position = (position specificity
-                                               (specificities object))
-                for i from 0 to list-position
-                for j = page-number-list then (cdr j)
-                collect (cond ((= i list-position) value)
-                              ((< i (length page-number-list))
-                               (car j))
-                              (t nil))))))
-
-(defgeneric specificity (page-object)
-  (:documentation "Returns the specificity of the page object.")
-  (:method ((object page))
-    (loop for page-number = (page-numbers object) then (cdr page-number)
-          for specificity in (cons nil (specificities object))
-          unless page-number return specificity
-          finally (return specificity))))
-
 (defgeneric ensure-book-specific (book specificity)
   (:documentation "Ensures that the book is at least as specific as specificity.
 Raises appropriate errors and gives common resolutions.")
   (:method ((book-obj page) specificity)
     (let ((specificity-spec (specificities book-obj))
           (page-number-list (page-numbers book-obj)))
+      (when (null specificity)
+        ;; All books are at least NIL specificity.
+        (return-from ensure-book-specific book-obj))
       (unless (member specificity specificity-spec)
         ;; Catch spec errors.
         (restart-case (error 'no-such-specificity
@@ -166,42 +194,41 @@ Raises appropriate errors and gives common resolutions.")
                            (list (read)))
             :report "Specify the return value for this function."
             (return-from ensure-book-specific return-value))))
-      (flet ((back-to-top (page spec)
-               ;; Restart the main loop to ensure everything is still sane.
-               (return-from main-loop (ensure-book-specific page spec))))
-        ;; Zip through the list to check for discrepancies and allow fixing.
-        (loop
-          named main-loop
-          for test-specificity in specificity-spec
-          for test-number = page-number-list then (cdr test-number)
-          for list-position from 1
-          unless (car test-number)
-          do (restart-case (error 'not-specific-enough
-                                  :current-specificity test-specificity
-                                  :target-specificity specificity)
-               (store-value (new-value)
-                 :interactive (lambda ()
-                                (format *query-io*
-                                        "~&Write in a new value for ~a: "
-                                        test-specificity)
-                                (list (parse-integer (read-line))))
-                 :report "Provide a value for the missing specificity."
-                 (setf (get-specificity book-obj test-specificity) new-value)
-                 (back-to-top book-obj specificity))
-               (store-all-values (new-values)
-                 :report "Put in new values of book/page/subpage all at once."
-                 :interactive (lambda ()
-                                (format *query-io*
-                                        "~&Write in order the values of ~a: "
-                                        specificity-spec)
-                                (list (read)))
-                 (setf (page-numbers book-obj) new-values)
-                 (back-to-top book-obj test-specificity)))
-          when (eql test-specificity specificity)
-          do (when (cdr test-number)
-               (warn "~s still remains in page-number list!"
-                     (cdr test-number)))
-          and return book-obj)))))
+      (loop
+        named main-loop
+        for test-specificity in specificity-spec
+        for test-number across page-number-list
+        unless test-number
+        do (flet ((back-to-top (page spec)
+                    ;; Restart the main loop to ensure everything is still sane.
+                    (return-from main-loop (ensure-book-specific page spec))))
+             (format t "Got here.")
+             (restart-case (error 'not-specific-enough
+                                 :current-specificity test-specificity
+                                 :target-specificity specificity)
+              (store-value (new-value)
+                :interactive (lambda ()
+                               (format *query-io*
+                                       "~&Write in a new value for ~a: "
+                                       test-specificity)
+                               (list (parse-integer (read-line))))
+                :report "Provide a value for the missing specificity."
+                (setf (get-specificity book-obj test-specificity) new-value)
+                (back-to-top book-obj specificity))
+              (store-all-values (new-values)
+                :report "Put in new values of book/page/subpage all at once."
+                :interactive (lambda ()
+                               (format *query-io*
+                                       "~&Write in order the values of ~a: "
+                                       specificity-spec)
+                               (list (read)))
+                (setf (page-numbers book-obj) new-values)
+                (back-to-top book-obj test-specificity))))
+        when (eql test-specificity specificity)
+        #|do (when #
+        (warn "~s still remains in page-number list!"
+        (cdr test-number)))
+        and |# return book-obj))))
 
 ;;; Restart functions
 (defun ignore-specificity-spec (condition)
@@ -213,64 +240,68 @@ Raises appropriate errors and gives common resolutions.")
   (when (eql :subpage (current-specificity condition))
     (invoke-restart 'store-value 1)))
 
-;;; Getters and setters for a specific page.
-(defgeneric book-path (object)
-  (:documentation "Makes the pathname for the book.
-Raises an error if the book is not specific enough.")
-  (:method :before ((object page))
-    ;; Assert that there is is enough information.
-    (handler-bind ((no-such-specificity #'ignore-specificity-spec)
-                   (warning #'muffle-warning))
-      (ensure-book-specific object :book)))
+;;; Operations
+;;; = = = = =
+;;; Determining the path.
+(defun expand-wild-pathname (wild-path &optional allow-multiple-files)
+  "Expands out a wild pathname and returns the first one.
+Warns if there isn't exactly one result returned,
+unless allow-multiple-files is non-nil."
+  (let ((matched-files (directory wild-path)))
+    (cond ((= 1 (length matched-files))
+           (first matched-files))
+          ((= 0 (length matched-files))
+           (warn "No items found for specified page number."))
+          (allow-multiple-files matched-files)
+          (t (warn "Too many items found for specified page number.")
+             (values (first matched-files)
+                     (rest matched-files))))))
+
+(defgeneric construct-boc-filename (book-object specificity &optional use-wild)
+  (:documentation "Constructs the filename off of an object.")
+  (:method ((book-object book-of-conworlds-page) specificity &optional use-wild)
+    (specificity-bind ((page :page) (subpage :subpage)) book-object
+      (cond ((eql specificity :book) (if use-wild :wild))
+            (t (when (and (eql specificity :subpage)
+                          (null subpage))
+                 (setf subpage 1))
+               (pathname-name
+                (format nil "~2,'0d~:[?~;~:*~c~]-*"
+                        page (number->letter subpage))))))))
+
+(defgeneric construct-folder-name (book-object)
+  (:documentation "Returns the folder name associated with the book.")
   (:method ((object book-of-conworlds-page))
-    (merge-pathnames
-     (make-pathname
-      :directory (list :relative (format nil "Book ~a"
-                                         (get-specificity object :book))))
-     (root object)))
+    (specificity-bind ((book :book)) object
+      (format nil "Book ~a" book)))
   (:method ((object dated-page))
-    (merge-pathnames
-     (make-pathname
-      :directory (list :relative (format-time (date-of-creation object))))
-     (root object))))
+    (format-time (date-of-creation object))))
 
-(defgeneric page-path-wild (page)
-  (:documentation "Makes the pathname for a specific page.
-Only returns the wild format. ")
-  (:method :before ((page-object page))
-    (handler-bind ((no-such-specificity #'ignore-specificity-spec))
-      (ensure-book-specific page-object :page)))
-  (:method ((page-object page-using-wildcards))
-    (let ((page (get-specificity page-object :page))
-          (subpage (get-specificity page-object :subpage)))
-      (merge-pathnames
-       (make-pathname
-        :name (pathname-name (format nil "~2,'0d~:[?~;~:*~c~]-*"
-                                     page (number->letter subpage)))
-        :type "jpg")
-       (book-path page-object)))))
-
-(defgeneric page-path (page)
-  (:documentation "Finds the pathname for a specific page.")
-  (:method :before ((page-object page))
-    (handler-bind ((no-such-specificity #'ignore-specificity-spec))
-      (ensure-book-specific page-object :page)))
-  (:method ((page-object page-using-wildcards))
-    (let ((search-results (directory (page-path-wild page-object))))
-      (cond ((= 1 (length search-results))
-             (first search-results))
-            ((= 0 (length search-results))
-             (warn "No items found for specified page number."))
-            (t (warn "Too many items found for specified page number.")
-               (values (first search-results)
-                       (rest search-results))))))
-  (:method ((object non-boc-page))
-    (merge-pathnames
-     (make-pathname
-      :directory (list :relative (format-time (date-of-creation object)))
-      :name (format nil "SCAN~4,'0d" (get-specificity object :page))
-      :type "jpg")
-     (root object))))
+(defgeneric path-up-to (object specificity &key use-wild expand-wild)
+  (:documentation "Returns the path up to the specificity.
+If use-wild is non-nil, then provide a wild pathname if relevant.")
+  (:method :before ((object page) specificity &key &allow-other-keys)
+    (handler-bind ((warning #'muffle-warning)
+                   (not-specific-enough #'implicit-subpage))
+      (ensure-book-specific object specificity)))
+  (:method ((object page) (specificity symbol)
+            &key use-wild expand-wild)
+    (specificity-bind ((book :book)) object
+      ;; Find the wild path in any case,
+      ;; as it is required to find the ordinary path.
+      (let ((wild-path
+              (merge-pathnames
+               (make-pathname
+                :directory (list :relative (construct-folder-name object))
+                :name (construct-boc-filename object specificity use-wild)
+                :type (if (eql specificity :book)
+                          (if use-wild :wild)
+                          "jpg"))
+               (root object))))
+        ;; Now decide if we bail out here.
+        (cond (use-wild wild-path)
+              ((or expand-wild (find specificity '(:page :subpage)))
+               (expand-wild-pathname wild-path)))))))
 
 ;;; Existence and Ignoredness
 (defgeneric book-exists-p (book)
