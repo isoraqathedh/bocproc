4e7d3d44f990bd62ead3d94011d971817934cea0
Isoraķatheð Zorethan <isoraqathedh.zorethan@gmail.com>
2016-03-01 23:08:53 +0000

    New package for reading BPC files.

--- package.lisp
+++ package.lisp
@@ -3,3 +3,7 @@
 (defpackage #:info.isoraqathedh.bocproc
   (:use #:cl #:thread-expr)
   (:nicknames #:bocproc))
+
+(defpackage #:info.isoraqathedh.bocproc.bpc-parser
+  (:nicknames #:bpc)
+  (:import-from #:bocproc #:version #:process-file #:*state*))
