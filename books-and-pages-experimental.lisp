4d2fd92b6365e4373f19580ac5efe194b8e0fa3e
Isoraķatheð Zorethan <isoraqathedh.zorethan@gmail.com>
2016-06-11 15:54:14 +0100

    Start defining classes.

--- books-and-pages-experimental.lisp
+++ books-and-pages-experimental.lisp
@@ -1,1 +1,23 @@
 (in-package :bocproc-exp)
+
+(defclass book-series ()
+  ((series
+    :type symbol
+    :initarg :series
+    :documentation "Series identifier. A keyword.")
+   (specificities
+    :initarg :specificities
+    :documentation #.(concatenate 'string
+                                  "An n×3 array"
+                                  " with the names of the specificities"
+                                  " and also their cutoffs. "
+                                  " The rows are symbol, min, max."))
+   (format
+    :initarg :format
+    :documentation "The format of the filenames of this series."))
+  (:documentation "Class for defining book series."))
+
+(defclass book-page (book-series)
+  ((page-numbers :initarg :page-numbes
+                 :initform (vector)
+                 :type (vector number))))
