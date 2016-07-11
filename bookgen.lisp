0259814b1b55c192dd8435887d973a59ac1ebc96
Isoraqathedh Zorethan <isoraqathedh.zorethan@gmail.com>
2016-07-11 11:25:04 +0800

    Missed a pair of brackets.

--- bookgen.lisp
+++ bookgen.lisp
@@ -234,9 +234,9 @@ The output can be one of these three:
 
 Modifies the generator, returns the new page.")
   (:method ((gen page-generator) (spec symbol))
-    (loop do (handler-bind (spec-out-of-bounds
-                            (lambda (condition)
-                              (invoke-restart 'carry)))
+    (loop do (handler-bind ((spec-out-of-bounds
+                              (lambda (condition)
+                                (invoke-restart 'carry))))
                (incf (point-specificity gen spec)))
           until (eql (point-status gen) :available))))
 
