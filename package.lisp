a2c2f7e134fe4c170fc0ec287a54b1f2a7c1e79c
Isoraqathedh Zorethan <isoraqathedh.zorethan@gmail.com>
2016-08-04 13:23:14 +0800

    Add documentation for the bpc package.

--- package.lisp
+++ package.lisp
@@ -8,4 +8,7 @@
 (defpackage #:info.isoraqathedh.bocproc.bpc-parser
   (:nicknames #:bpc)
   (:export #:version #:process-file #:loud)
-  (:import-from #:cl #:nil #:t))
+  (:import-from #:cl #:nil #:t)
+  (:documentation "Special package for .bpc files.
+
+Symbols in this package is available for use in bocproc files."))
