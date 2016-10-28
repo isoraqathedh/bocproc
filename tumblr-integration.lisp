a280388bb9ef7cd49461873dd2f9c36b4b407a3a
Isoraķatheð Zorethan <isoraqathedh.zorethan@gmail.com>
2016-10-28 23:36:28 +0100

    More extractor functions.

--- tumblr-integration.lisp
+++ tumblr-integration.lisp
@@ -12,3 +12,22 @@
     :caption comments
     :tags tags)
    (config :blog)))
+
+(defun tumblr-photos-size (photo)
+  "Retrieve the image URL of the largest image in a single image object."
+  (humbler:url
+   (first ; The first size is the original one
+    (humbler:sizes photo))))
+
+(defun photo-URLs (post)
+  "Retrieve the image URLs of the tumblr post."
+  (mapcar #'tumblr-photos-size (humbler:photos post)))
+
+(defun construct-tumblr-post (images caption tags)
+  "Create a tumblr image post using the Humbler API."
+  (humbler:blog/post-photo )
+  (make-instance
+   'humbler:photo-post
+   :photos (make-instance 'humbler:photo )
+   :state :queue
+   :caption ))
