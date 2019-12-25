9118033a91c3db2d74c45c7864c956827585a576
Isoraķatheð Zorethan <isoraqathedh.zorethan@gmail.com>
2019-12-26 02:59:47 +0800

    Split out and make concrete the stable entity.

--- classes.lisp
+++ classes.lisp
@@ -3,11 +3,23 @@
 (defvar +bocproc-uuid+
   (uuid:make-uuid-from-string "87832309-5EE5-48D0-B2C7-41E88531B360"))
 
-(defclass affinity ()
+(defclass stable-entity ()
+  ((slug-symbol :accessor slug-symbol
+                :initarg :slug-symbol
+                :type 'symbol)))
+
+(defmethod print-object :around ((object stable-entity) stream)
+  (print-unreadable-object (object stream :type t)
+    (call-next-method)))
+
+(defmethod print-object ((object stable-entity) stream)
+  (format stream "~s" (slug-symbol object)))
+
+(defclass affinity (stable-entity)
   ((name :accessor name
          :initarg :name)))
 
-(defclass tag ()
+(defclass tag (stable-entity)
   ((name :accessor name
          :initarg :name)
    (affinity :accessor affinity
