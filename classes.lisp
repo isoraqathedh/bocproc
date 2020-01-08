f8af52dc6b12edf0ab9a806c5783aef22be3123c
Isoraķatheð Zorethan <isoraqathedh.zorethan@gmail.com>
2020-01-09 07:39:17 +0800

    Accommodate tags and create slug syntax.

--- classes.lisp
+++ classes.lisp
@@ -3,6 +3,8 @@
 (defvar +bocproc-uuid+
   (uuid:make-uuid-from-string "87832309-5EE5-48D0-B2C7-41E88531B360"))
 
+(defvar +alphabet+ "abcdefghijklmnopqrstuvwxyz")
+
 (defclass stable-entity ()
   ((slug-symbol :accessor slug-symbol
                 :initarg :slug-symbol
@@ -14,17 +16,52 @@
 
 (defparameter *stable-entities* ())
 
-(defun define-stable-entity (thing name)
-  (let ((new-thing (make-instance thing :slug-symbol name)))
-    (pushnew new-thing *stable-entities* :key #'slug-symbol)
-    new-thing))
+(defun push-stable-entity (stable-entity)
+  (pushnew stable-entity *stable-entities* :key #'slug-symbol))
 
-(defun get-stable-entity (name)
-  (find name *stable-entities* :key #'slug-symbol))
+(defun get-stable-entity (name &optional type)
+  (let ((thing (find name *stable-entities* :key #'slug-symbol)))
+    (if (null type) thing
+        (and (typep thing type) thing))))
 
 (defclass affinity (stable-entity)
   ((name :accessor name
          :initarg :name)))
+
+(defun simple-character-p (character)
+  (find character (concatenate 'string
+                       (string-upcase +alphabet+)
+                       "0123456789-_")))
+
+(defun slugify (string)
+  (let* ((slug-package '#:info.isoraqathedh.bocproc.entities)
+         (slug-symbol
+           (thread-expr:with-expression-threading (x)
+             (string-upcase string)
+             (substitute #\- #\Space x)
+             (substitute-if-not #\_ #'simple-character-p x)
+             (string-trim "-" x)
+             (or (find-symbol x slug-package)
+                 (intern x slug-package)))))
+    (export slug-symbol slug-package)
+    slug-symbol))
+
+(defun push-stable-entity (name slug type &rest type-initargs)
+  (let ((true-slug (etypecase slug
+                     (null (slugify name))
+                     (string (slugify slug))
+                     (symbol slug)))
+        instance)
+    (or (get-stable-entity true-slug type)
+        (progn
+          (setf instance (apply #'make-instance type :name name
+                                                     :slug true-slug
+                                                     type-initargs))
+          (push instance *stable-entities*)
+          instance))))
+
+(defun define-affinity (name &optional slug)
+  (push-stable-entity name slug 'affinity))
 
 (defclass tag (stable-entity)
   ((name :accessor name
@@ -34,6 +71,15 @@
              :initarg :affinity)
    (other-properties :initform ()
                      :initarg :props)))
+
+(defun define-tag (name affinity &optional slug)
+  (let ((affinity (get-stable-entity affinity 'affinity))
+        (true-slug (etypecase slug
+                     (null (slugify name))
+                     (string (slugify slug))
+                     (symbol slug))))
+    (or (get-stable-entity true-slug 'tag)
+        )))
 
 (defmethod print-object ((object tag) stream)
   (print-unreadable-object (object stream :type t)
