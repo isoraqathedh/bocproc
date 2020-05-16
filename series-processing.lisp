1f7519f9be86be84dc253f0025abe47e48e9ac76
Isoraķatheð Zorethan <isoraqathedh.zorethan@gmail.com>
2020-05-16 13:40:08 +0800

    Write more modifiers for page numbers.

--- series-processing.lisp
+++ series-processing.lisp
@@ -28,16 +28,37 @@ which is out of bounds~:[~; ~:*(was ~a)~]"
       page-number
       (error 'page-out-of-bounds-error :attempted page-number)))
 
+(defun parse-spec (page-number spec)
+  (cons (etypecase spec
+          ((or null number) (caar (last (numbers page-number))))
+          (cons (car spec))
+          (symbol spec))
+        (etypecase spec
+          ((or null symbol) 1)
+          (cons (cdr spec))
+          (number spec))))
+
 (defun next (page-number &optional spec)
-  (let ((actual-spec (or spec (caar (last (numbers page-number))))))
-    (incf (cdr (assoc actual-spec (numbers page-number))))
+  (destructuring-bind (spec-symbol . amount) (parse-spec page-number spec)
+    (incf (cdr (assoc spec-symbol (numbers page-number))) amount)
     page-number))
 
 (defun previous (page-number &optional spec)
-  (let ((actual-spec (or spec (caar (last (numbers page-number))))))
-    (decf (cdr (assoc actual-spec (numbers page-number))))
+  (destructuring-bind (spec-symbol . amount) (parse-spec page-number spec)
+    (decf (cdr (assoc spec-symbol (numbers page-number))) amount)
     page-number))
 
+(defgeneric page-number-list (page-number)
+  (:method ((page-number page-number))
+    (mapcar #'cdr (numbers page-number))))
+
+(defgeneric (setf page-number-list) (value page-number)
+  (:method ((value list) (page-number page-number))
+    (loop for num in value
+          for old-number in (numbers page-number)
+          do (setf (cdr old-number) num))
+    value))
+
 ;;; Converting page numbers to pages
-(defun fill (page-number))
+(defun grab-data (page-number))
 (defun available (page))
